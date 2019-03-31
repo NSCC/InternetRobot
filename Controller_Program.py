a86adb5ac4eac034dc4214af1df9bbe44e185e12
Ashton Ford <32301633+madilla111@users.noreply.github.com>
2019-03-31 14:55:57 -0300

    Update Controller_Program.py
    Added control to turn.

--- Controller_Program.py
+++ Controller_Program.py
@@ -1,11 +1,7 @@
-# Importing the socket library to use for the transmission and receiving of UDP strings
-import socket
 import serial
-import time
 from evdev import InputDevice, categorize, ecodes
 
-# The address used to receive UDP strings
-# The port that will be used to receive UDP strings
+
 ser = serial.Serial('/dev/ttyS0',9600,timeout = 1)
 safe_forward = 44
 safe_reverse = 84
@@ -13,10 +9,13 @@ safe_reverse = 84
 l_stop = 64
 r_stop = 191
 left = l_stop
+leftOld = 0
+rightOld = 0
 change = 0 
 right = r_stop
 ALL_STOP = 0
 fail_safe = "STOP"
+direction = ""
 
 gamepad = InputDevice('/dev/input/event0')
 aBtn = 304
@@ -27,23 +26,9 @@ R1 = 311
 L1 = 310
 R2 = 313
 L2 = 312
+LSX = 00
+LSXVAL = 128
 
-#def udp_read():
-#	DirectionDictionary = {"LEFT": "ThingL", "FORWARD": "ThingF", "BACK": "ThingB", "RIGHT": "ThingR","STOP": "ThingS"}
-#	data, address = UDPSocket.recvfrom(1024)
-#	packet = data.decode().upper()
-#	if packet in DirectionDictionary:
-#		print(packet)
-#		return packet
-# Defining the dictionary that will house the key/value pairs for the controls
-#DirectionDictionary = {"LEFT": LEFT, "FORWARD": FORWARD, "BACK": BACK, "RIGHT": RIGHT,"STOP": STOP}
-# Creating a socket object, using AF_INET(IPV4) and SOCK_DGRAM (socket type constant)
-#UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Binding the socket object to a pre-defined address and port
-#UDPSocket.bind((UDP_IP_Address, UDP_Port_Number))
-
-#function to adjust velocity
 def inertia(Mvalue, Mrestrict): #Mrestrict is the integer that you do not want the result to exceed
 	rate = 0.25 #the rate at which we will adjust the speed. 
 	start_value = Mvalue - Mrestrict;
@@ -77,18 +62,37 @@ def FORWARD():
 	return 1
 
 	#fucntion to turn left
-def LEFT(left, right):
-	#reduce speed of Motor 1
-	left = inertia(left, l_stop)
-	ser.write( bytes([left]) )
+def LEFT(direction):
+	global left
+	if direction == "f":
+		left = 30
+		ser.write( bytes([left]) )
+	else:
+		left = 101
+		ser.write( bytes([left]) )
 	print("M1 = ", left)
 	return 1
 
+def LEFTS(leftOld):
+	global left
+	left = leftOld
+	ser.write( bytes([left]) )
+	
+def RIGHTS(rightOld):
+	global right
+	right = rightOld
+	ser.write( bytes([right]) )
+	
 #function to turn right
-def RIGHT(left, right):
+def RIGHT(direction):
 	#reduce speed of Motor 2
-	right = inertia(right, r_stop)
-	ser.write( bytes([right]))
+	global right
+	if direction == "f":
+		right = 145
+		ser.write( bytes([right]))
+	else:
+		right = 228
+		ser.write( bytes([right]))
 	print("M2 = ", right)
 	return 1
 
@@ -106,49 +110,17 @@ def STOP():
 	print("M2 = ", right)
 	print("ALL STOP")
 	return 1
-#function that calls on functions
-def switch_func(Data):
-	#search for Packet Value in DirectionDictionary
-	DirectionDictionary = {"LEFT": LEFT, "FORWARD": FORWARD, "BACK": BACK, "RIGHT": RIGHT,"STOP": STOP}
-	func = DirectionDictionary.get(Data)
-	return func(left, right)
 
-#Data = udp_read()
-# While true loop to continuously listen for UDP transmissions
 while True:
-	# recvfrom receives a pair of values, string & address, where string is the string representing the data being sent
-	# & address is the address of the socket sending the data
-	# All of the above is preceded by a number of bytes to receive from a transmission
-	# Example = socket.recvfrom(BufferSize[,flags])
-	#data, address = UDPSocket.recvfrom(1024)
-	# As long as the transmission isn't empty
-	# Create a string from the transmission of type byte and capitalize its letters
-	#Data = udp_read()
 
-	#function that calls on functions
-	#def switch_func(Data):
-	#	#search for Packet Value in DirectionDictionary
-	#	DirectionDictionary = {"LEFT": LEFT, "FORWARD": FORWARD, "BACK": BACK, "RIGHT": RIGHT,"STOP": STOP}
-	#	func = DirectionDictionary.get(Data)
-	#	return func(left, right)
-
-	#print("Packet: ",Data)
-	#change = switch_func(Data)
-	#if change == 1:
-		#fail_safe = Data
-		#print("Change = ", change)
-		#Data = udp_read()
-	#else:
-		#Data = fail_safe
-		#change = switch_func(Data)
 		
 	for event in gamepad.read_loop():
-    #filters by event type
+
 		if event.type == ecodes.EV_KEY:
+					print(LSXVAL)							
 					if event.value == 1:
 						if event.code == aBtn:
 							print("A Pressed")
-							FORWARD()
 						elif event.code == bBtn:
 							print("B Pressed")
 							STOP()
@@ -156,22 +128,34 @@ while True:
 							print("X Pressed")
 						elif event.code == yBtn:
 							print("Y Pressed")
-							BACK()
 						elif event.code == R1:
 							print("R1 Pressed")
 						elif event.code == R2:
+							FORWARD()
 							print("R2 Pressed")
 						elif event.code == L1:
 							print("L1 Pressed")
 						elif event.code == L2:
-							print("L2 Pressed")
-						
+							BACK()
+														
+					if event.value == 2:
+						if event.code == R2:
+							if LSXVAL == 128:
+								FORWARD()
+								direction = "f"
+						if event.code == L2:
+							if LSXVAL == 128:
+								BACK()
+								direction = "b"
 					if event.value == 0:
 						if event.code == aBtn:
 							print("A Released")
+							STOP()
+							direction = ""
 						elif event.code == bBtn:
 							print("B Released")
 							STOP()
+							direction = "f"
 						elif event.code == xBtn:
 							print("X Released")
 						elif event.code == yBtn:
@@ -179,8 +163,27 @@ while True:
 						elif event.code == R1:
 							print("R1 Pressed")
 						elif event.code == R2:
-							print("R2 Pressed")
+							STOP()
+							direction = "f"
 						elif event.code == L1:
 							print("L1 Pressed")
 						elif event.code == L2:
-							print("L2 Pressed")
+							STOP()
+							direction = "f"
+							
+		if event.type == ecodes.EV_ABS:
+					if event.code == LSX:
+						LSXVAL = event.value
+						if event.value == 0:
+							leftOld = left
+							LEFT(direction)
+						if event.value == 128:
+							LEFTS(leftOld)
+						if event.value == 255:
+							rightOld = right
+							RIGHT(direction)
+						if event.value == 128:
+							RIGHTS(rightOld)
+						
+
+							
